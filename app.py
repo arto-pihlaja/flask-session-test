01371828f02fa351a3629257961b5d0c3144b3da
Arto Pihlaja <arto.pihlaja@gmail.com>
2023-06-19 15:39:38 +0000

    manage session with uid

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 # Building atop the simple example from https://www.geeksforgeeks.org/how-to-use-flask-session-in-python-flask/
 from flask import Flask, render_template, redirect, request, session
 # The Session instance is not used for direct access, you should always use flask.session
-from flask_session import Session
+#from flask_session import Session
 from werkzeug.utils import secure_filename
 import pickle 
 import os
+from uuid import uuid4 
 
 if os.path.exists('./pickles'):
     pass
@@ -15,7 +16,7 @@ app = Flask(__name__, template_folder='.')
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 app.secret_key = 'DefineLytcheSala'
-Session(app)
+#Session(app)
 
 class UserData:
     def __init__(self, uname) -> None:
@@ -28,25 +29,26 @@ class UserData:
 @app.route("/")
 def index():
     ss = session._get_current_object()
-    nm = session.get("name")
-    if not nm:
+    uid = session.get("uid")
+    # Flask sessions expire when the browser is closed, which is fine https://flask.palletsprojects.com/en/2.3.x/api/#flask.session.permanent
+    if not uid:
         return redirect("/login")
     else:           
-        with open(os.path.join('./pickles', ss.sid), 'rb') as f:     
-            ud = pickle.load(f)
-    return render_template('index.html', context=ud)
+        with open(os.path.join('./pickles', str(uid)), 'rb') as f:     
+            ud = pickle.load(f)            
+    return render_template('index.html', ud=ud, sid=str(uid))
  
  
 @app.route("/login", methods=["POST", "GET"])
 def login():
-    if request.method == "POST":
-        ss = session._get_current_object()
+    if request.method == "POST":        
         nm = request.form.get("name")
         uf = request.files['ufile'].filename
         ud = UserData(nm)
         ud.set_file(uf)
-        session["name"] = nm
-        with open(os.path.join('./pickles', ss.sid), 'wb') as f:
+        uid = uuid4()
+        session["uid"] = uid
+        with open(os.path.join('./pickles', str(uid)), 'wb') as f:
             pickle.dump(ud, f)
         return redirect("/")
     return render_template("login.html")
